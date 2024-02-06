ae798bc3515c9c0ba7a5ac0a75b84fbea4f860d1
Marquito92 <146921836+Marquito92@users.noreply.github.com>
2024-02-06 17:04:55 +0100

    Add files via upload

---
+++ burger.js
@@ -0,0 +1,6 @@
+
+// Fonction pour basculer l'affichage du menu déroulant
+function toggleMenu() {
+    const navMenu = document.querySelector('.nav-menu');
+    navMenu.classList.toggle('show');
+}
